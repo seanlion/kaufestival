22cb707e02738ca255b716428b4dd91e92b2598a
Seung Huh <skyhs3507@likelion.org>
2017-09-13 17:08:35 +0000

    js 완성 일정,프로그램완성

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+ /*global $*/ 
+  $('.btn1').on('click', function(){
+      var target = $(this).attr('rel');
+      $('.btn1').css("background-color","#EFE7E6");
+      $('.btn1').css("border-color","#EFE7E6");
+      $('.btn1').css("color","#662408");
+      $(this).css("background-color","#662408");
+      $(this).css("border-color","#662408");
+      $(this).css("color","#EFE7E6");
+      // $("#sch1").toggle();
+      // $("#sch2").toggle();
+      $("#"+target).show().siblings().hide();
+      // $("#"+target).siblings().hide();
+     
+   });
+ 
+ 
+  $('.btn2').on('click', function(){
+      var target2 = $(this).attr('rel');
+      $('.btn2').css("background-color","#EFE7E6");
+      $('.btn2').css("border-color","#EFE7E6");
+      $('.btn2').css("color","#662408");
+      $(this).css("background-color","#662408");
+      $(this).css("border-color","#662408");
+      $(this).css("color","#EFE7E6");
+      // $("#sch1").toggle();
+      // $("#sch2").toggle();
+      $("#"+target2).show().siblings(".row").hide();
+   });
+ 
+ 
+ 
+ /이건 걍 무시 나중에 참고/ 
+ 
+ // $('.btn1').on('click', function(){
+ //      var target = $(this).attr('rel');
+ //      $('.btn1').css("background-color","#EFE7E6");
+ //      $('.btn1').css("border-color","#EFE7E6");
+ //      $('.btn1').css("color","#662408");
+ //      $(this).css("background-color","#662408");
+ //      $(this).css("border-color","#662408");
+ //      $(this).css("color","#EFE7E6");
+ //      // $("#sch1").toggle();
+ //      // $("#sch2").toggle();
+ //      $("#sch1").attr("src", "/asset/test3-" + target + ".png");
+ //      // $("#"+target).siblings().hide();
+     
+ //   });
+ 
+ 
